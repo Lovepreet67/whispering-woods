68ab896dc44a77b29c2d962c0e01caf85dde71f3
lovepreet67 <getupyou98@gmail.com>
2025-10-23 19:31:41 +0530

    updated start.sh script to use certifcates and keys

--- start.sh
+++ start.sh
@@ -32,15 +32,90 @@ cargo build --release
 echo "Starting NameNode..."
 cargo run -p namenode >/dev/null 2>&1 < /dev/null &
 
-sleep 5
+sleep 10
+
+# We will login to the namenode and get jwt to generate certifcates for nodes
+NAMENODE_URL="http://localhost:8080"
+NAMENODE_USERNAME="username"
+NAMENODE_PASSWORD="password"
+
+echo "Logging in..."
+LOGIN_RESPONSE=$(curl -s -X POST "$NAMENODE_URL/auth/login" \
+  -H "Content-Type: application/json" \
+  -d "{\"username\": \"$NAMENODE_USERNAME\", \"password\": \"$NAMENODE_PASSWORD\"}")
+
+echo "Logging response..."
+TOKEN=$(echo "$LOGIN_RESPONSE" | jq -r '.Token')
+if [[ "$TOKEN" == "null" || -z "$TOKEN" ]]; then
+  echo "Failed to extract token from login response"
+  echo "$LOGIN_RESPONSE"
+  exit 1
+fi
+
+echo "Login Successfull."
 
 # Run DataNode 1 (client port 8000, internal 3000)
+NODE_ID=datanode_1
 echo "Starting DataNode 1..."
+echo "Requesting certificate for node '$NODE_ID'..."
+  CERT_RESPONSE=$(curl -s -X POST "$NAMENODE_URL/cert/issue" \
+    -H "jwt_token: $TOKEN" \
+    -H "auth_type: JwtTokenAuth" \
+    -H "Content-Type: application/json" \
+    -d "{\"node_id\": \"$NODE_ID\", \"node_type\":\"Datanode\"}")
+
+CERT=$(echo "$CERT_RESPONSE" | jq -r '.cert')
+SECKEY=$(echo "$CERT_RESPONSE" | jq -r '.key')
+if [[ "$CERT" == "null" || -z "$CERT" ]]; then
+  echo "Failed to extract cert from response"
+  echo "$CERT_RESPONSE"
+  continue
+fi
+echo "Certificate received "
+
+# ---- APPEND RAW CERT TO YAML CONFIG ----
+echo "Appending certificate"
+
+cat <<EOF >> "$(pwd)/datanode/config/default.yaml"
+namenode_cert: $(echo "$CERT" | sed 's/^/  /')
+secret_key: $(echo "$SECKEY" | sed 's/^/  /')
+EOF
+
+echo "Certificate appended successfully."
+
 cargo run -p datanode >/dev/null 2>&1 < /dev/null&
 
 # Optional delay to let nodes start
 sleep 2
 
+
+echo "Starting a client"
+echo "Getting cert for client"
+CERT_RESPONSE=$(curl -s -X POST "$NAMENODE_URL/cert/issue" \
+    -H "jwt_token: $TOKEN" \
+    -H "auth_type: JwtTokenAuth" \
+    -H "Content-Type: application/json" \
+    -d "{\"node_id\": \"client_0\", \"node_type\":\"Client\"}")
+
+  CERT=$(echo "$CERT_RESPONSE" | jq -r '.cert')
+  SECKEY=$(echo "$CERT_RESPONSE" | jq -r '.key')
+
+  if [[ "$CERT" == "null" || -z "$CERT" ]]; then
+   echo "Failed to extract cert from response"
+   echo "$CERT_RESPONSE"
+   continue
+  fi
+ echo "Certificate received "
+
+# ---- APPEND RAW CERT TO YAML CONFIG ----
+echo " Appending client certificate"
+
+cat <<EOF >> "$(pwd)/client/config/default.yaml"
+namenode_cert: $(echo "$CERT" | sed 's/^/  /')
+secret_key: $(echo "$SECKEY" | sed 's/^/  /')
+EOF
+
+echo "Client Certificate appended successfully."
 # Run Client (in foreground)
 echo "Starting Client..."
 cargo run -p client 
